0884cc7741c1d401d973ac6556ad02f633b544f5
paine1690 <paine1690@126.com>
2016-03-16 19:05:40 +0800

    计数排序
    计数排序，用于特定条件下，不是基于比较的，可以达到O(n+k)

--- src/Sort/Sort.java
+++ src/Sort/Sort.java
@@ -1,9 +1,52 @@
 package Sort;
+import java.util.Arrays;
 
 public class Sort {
-
+	/*
+	 * 计数排序
+	 * 1、不是基于比较的，复杂度为O(k+n)
+	 * 2、前提必须是带排序元素范围已知(K)
+	 * 3、稳定排序
+	 * 4、需要一个辅助数组C[k]
+	 */
+	public static int[] countingSort_extra(int nums[], int k){
+		int n=nums.length;
+		int[] re=new int[n];
+		int[] C=new int[k+1];
+		for(int i=0; i<n; i++){
+			C[nums[i]]++;
+		}
+		for(int i=1; i<C.length; i++){
+			C[i]+=C[i-1];
+		}
+		for(int i=0; i<n; i++){
+			re[C[nums[i]]-1]=nums[i];
+			C[nums[i]]--;
+		}		
+		return re;
+	}
+	/*
+	 * 计数排序的改进，排序后直接写会原来的数组
+	 */
+	public static void countingSort(int[] nums, int k){
+		int n=nums.length;
+		int[] B=new int[k+1];
+		int count=0;
+		for(int i=0; i<n; i++){
+			B[nums[i]]++;
+		}
+		for(int i=0; i<=k; i++){
+			while(B[i]-->0){
+				nums[count++]=i;
+			}
+		}
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int[] a={2,5,3,0,2,3,0,3};
+		countingSort(a, 5);
+		System.out.println(Arrays.toString(a));
 	}
 
 }
