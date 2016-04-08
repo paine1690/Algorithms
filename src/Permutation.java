59e559d7d4f27cc0329bde0c06309ae23251ea1e
paine1690 <paine1690@126.com>
2016-04-08 10:58:46 +0800

    全排列问题
    递归实现

---
+++ src/Permutation.java
@@ -0,0 +1,67 @@
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 		全排列问题
+ * 
+ * @author Paine
+ *
+ */
+public class Permutation {
+
+	private static void swap(int[] nums, int a, int b){
+		int temp=nums[a];
+		nums[a]=nums[b];
+		nums[b]=temp;
+	}
+	
+	
+	public static void permute(int[] nums){
+		perm(nums, 0, nums.length-1);
+		perm2(nums, 0, nums.length-1);
+	}
+	
+	//1、递归实现
+	private static void perm(int[] nums, int start, int end){
+		if(start==end){
+			System.out.println(Arrays.toString(nums));
+		}else{
+			for(int i=start; i<=end; i++){
+				swap(nums, start, i);
+				perm(nums, start+1, end);
+				swap(nums, start, i);
+			}
+		}
+	}
+	
+	//2、递归实现 防止重复元素  第i个数与第j个数交换时，要求[i,j)中没有与第j个数相等的数
+	private static boolean isSwap(int[] nums, int i, int j){
+		for(int k=i; k<j; k++){
+			if(nums[k]==nums[j]){
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	private static void perm2(int[] nums, int start, int end){
+		if(start==end){
+			System.out.println(Arrays.toString(nums));
+		}else{
+			for(int i=start; i<=end; i++){
+				if(isSwap(nums, start, i)){
+					swap(nums, start, i);
+					perm2(nums, start+1, end);
+					swap(nums, start, i);
+				}
+			}
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		int[] nums={1,2,2};
+		permute(nums);
+	}
+
+}
