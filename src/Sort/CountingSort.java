b1e92098b454a3b163b953eff6bdf4263e992d37
paine1690 <paine1690@126.com>
2016-05-24 17:12:28 +0800

    排序：基数排序

--- src/Sort/CountingSort.java
+++ src/Sort/CountingSort.java
@@ -1,23 +1,22 @@
-package Sort;
+package sort;
+
+import java.util.Arrays;
 
 /**
  * 
  * 		计数排序
- * 1、不是基于比较的，复杂度为O(k+n)
- * 2、前提必须是带排序元素范围已知(K)
- * 3、稳定排序
- * 4、需要一个辅助数组C[k]
+ * 
+ * 不是基于比较的，复杂度为O(k+n)
+ * 前提必须是带排序元素范围已知(K)
+ * 稳定排序
+ * 需要一个辅助数组C[k]
+ * 
+ * 改进后的虽然节省空间，但是无法保证稳定
  * 
  * @author Paine
  *
  */
-/*
- * 计数排序
- * 1、不是基于比较的，复杂度为O(k+n)
- * 2、前提必须是带排序元素范围已知(K)
- * 3、稳定排序
- * 4、需要一个辅助数组C[k]
- */
+
 
 public class CountingSort {
 	
@@ -31,14 +30,15 @@ public class CountingSort {
 		for(int i=1; i<C.length; i++){
 			C[i]+=C[i-1];
 		}
-		for(int i=0; i<n; i++){
+		for(int i=n-1; i>=0; i--){//必须从后向前遍历，才能保证稳定
 			re[C[nums[i]]-1]=nums[i];
 			C[nums[i]]--;
 		}		
 		return re;
 	}
+	
 	/*
-	 * 计数排序的改进，排序后直接写会原来的数组
+	 * 计数排序的改进，排序后直接写回原来的数组，但是不能保证稳定
 	 */
 	public static void countingSort(int[] nums, int k){
 		int n=nums.length;
@@ -55,7 +55,11 @@ public class CountingSort {
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int[] nums={1,5,4,6,3,2,1,4,8,9,6,5,4,5,2,4,8,2};
+		countingSort(nums, 9);
+		System.out.println(Arrays.toString(countingSort_extra(nums, 9)));
+		
+		
 	}
 
 }
