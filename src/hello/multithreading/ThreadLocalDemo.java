79e011f227cc84cf12501c492b26c053dcb3f41f
paine1690 <paine1690@126.com>
2016-12-26 15:41:56 +0800

    ThreadLocal

--- src/hello/multithreading/ThreadLocalDemo.java
+++ src/hello/multithreading/ThreadLocalDemo.java
@@ -32,36 +32,5 @@ public class ThreadLocalDemo {
 			new Thread(new MyThread(i)).start();
 		}
 	}
-	
-	
-	
-	
-	/*
-	 * 下面是Thread类的源码
-	 */
-	
-    /* ThreadLocal values pertaining to this thread. This map is maintained
-     * by the ThreadLocal class. */
-    ThreadLocal.ThreadLocalMap threadLocals = null;
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 }
+
